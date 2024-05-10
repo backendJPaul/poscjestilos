ac0f7d01dc7a010eff51c4eca1dd14c892c334b0
backendJPaul <files.pticlavilca@gmail.com>
2024-05-10 00:27:50 -0500

    add files

---
+++ src/main/java/com/jpaul/poscjestilos/PoscjestilosApplication.java
@@ -0,0 +1,13 @@
+package com.jpaul.poscjestilos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoscjestilosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoscjestilosApplication.class, args);
+	}
+
+}
